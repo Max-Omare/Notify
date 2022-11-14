d4c282e091108bf8679b8052138adfeefbb178a3
Ochieng Otieno <ochiengotieno71@gmail.com>
2022-11-14 12:35:39 +0300

    first commit

---
+++ db/migrate/20221114091829_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :school
+      t.string :reg_number
+      t.string :phone
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
